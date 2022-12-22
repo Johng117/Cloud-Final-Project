ca1101d99ad7f2c1e3a68d59ee20e003fa49337a
Johng117 <83654331+Johng117@users.noreply.github.com>
2022-12-22 21:45:05 +0000

    Added bastion resource and internet gateway
    Added bastion host EC2 resource and route in route table to connect to internet gateway.

--- main.tf
+++ main.tf
@@ -12,7 +12,56 @@ provider "aws" {
     region = var.AWS_REGION
 }
 
+resource "aws_instance" "fp_bastion" {
+    ami = var.IMAGE
+    instance_type = "t2.micro"
+    key_name = var.KEY_PAIR
+    vpc_security_group_ids = [aws_security_group.fp_bastion_sg.id]
+    subnet_id = aws_subnet.fp_public_subnet.id
+    associate_public_ip_address = true
 
+    tags = {
+        Name ="fp_bastion"
+    }
+}
+
+resource "aws_security_group" "fp_bastion_sg" {
+    name = "fp_bastion_sg"
+    description = "security group for bastion host"
+    vpc_id = aws_vpc.fp_vpc.id
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "HTTPS"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["86.149.44.13/32"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+    tags = {
+        Name = "fp_bastion_sg"
+    }
+}
 
 
 # resource "aws_db_instance" "fp_db" {
@@ -29,3 +78,4 @@ provider "aws" {
 #   publicly_accessible    = true
 #   skip_final_snapshot    = true
 # }
+
--- vpc.tf
+++ vpc.tf
@@ -9,13 +9,50 @@ resource "aws_vpc" "fp_vpc" {
 }
 
 # create a private subnet within vpc
-resource "aws_subnet" "fp_private_subnet" {
+# resource "aws_subnet" "fp_private_subnet" {
+#   vpc_id     = aws_vpc.fp_vpc.id
+#   cidr_block = "10.0.0.0/16"
+
+#   tags = {
+#     Name = "fp_private_subnet"
+#   }
+# }
+
+#create a public subnet within vpc
+resource "aws_subnet" "fp_public_subnet" {
   vpc_id     = aws_vpc.fp_vpc.id
   cidr_block = "10.0.0.0/16"
-  enable_dns_support = "true"
-  enable_dns_hostnames = "true"
 
   tags = {
-    Name = "fp_private_subnet"
+    Name = "fp_public_subnet"
   }
 }
+
+resource "aws_internet_gateway" "fp_ig" {
+    vpc_id = aws_vpc.fp_vpc.id
+    tags = {
+        Name = "fp_ig"
+    }
+}
+
+resource "aws_route" "fp_ig_route" {
+  route_table_id         = aws_vpc.fp_vpc.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.fp_ig.id
+}
+
+# resource "aws_route_table" "fp_rt" {
+#   vpc_id = aws_vpc.fp_vpc.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.fp_ig.id
+#   }
+#   tags = {
+#     Name = "fp_rt"
+#   }
+# }
+
+# resource "aws_route_table_association" "fp_rt" {
+#   subnet_id      = aws_subnet.fp_public_subnet.id
+#   route_table_id = aws_route_table.fp_rt.id
+# }
