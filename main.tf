e492360f765956256bb9dcb88a4f536cc13ce330
Johng117 <83654331+Johng117@users.noreply.github.com>
2023-01-31 15:40:45 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,21 +29,21 @@ module "ec2-bastion" {
 }
 
 module "ec2-api" {
-  source       = "./modules/ec2-api"
-  api-user-data    = templatefile("./data/fp_api.sh", { db = "${var.DB}", host = "${var.HOST}", password = "${var.DB_PASSWORD}", user = "${var.DB_USERNAME}" })
-  api-subnet-id    = module.public_2.public-2-id
-  api-sg-id        = module.sg_api.sg-api-id
-  image        = var.IMAGE
-  api-key-pair = var.API_KEY_PAIR
+  source        = "./modules/ec2-api"
+  api-user-data = templatefile("./data/fp_api.sh", { db = "${var.DB}", host = "${var.HOST}", password = "${var.DB_PASSWORD}", user = "${var.DB_USERNAME}" })
+  api-subnet-id = module.public_2.public-2-id
+  api-sg-id     = module.sg_api.sg-api-id
+  image         = var.IMAGE
+  api-key-pair  = var.API_KEY_PAIR
 }
 
 module "ec2-app" {
-  source       = "./modules/ec2-app"
-  app-user-data    = templatefile("./data/fp_app.sh", { api-ip = "${var.API_IP}" })
-  app-subnet-id    = module.public_3.public-3-id
-  app-sg-id        = module.sg_app.sg-app-id
-  image        = var.IMAGE
-  app-key-pair = var.APP_KEY_PAIR
+  source        = "./modules/ec2-app"
+  app-user-data = templatefile("./data/fp_app.sh", { api-ip = "${var.API_IP}" })
+  app-subnet-id = module.public_3.public-3-id
+  app-sg-id     = module.sg_app.sg-app-id
+  image         = var.IMAGE
+  app-key-pair  = var.APP_KEY_PAIR
 }
 
 
