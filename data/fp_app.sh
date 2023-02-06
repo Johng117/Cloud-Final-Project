355bec103e4844775d97e00efc3db9f91744ff8b
Johng117 <83654331+Johng117@users.noreply.github.com>
2023-02-06 19:39:52 +0000

    Update fp_app.sh

--- data/fp_app.sh
+++ data/fp_app.sh
@@ -6,7 +6,6 @@ sudo npm install -g forever
 sudo apt install -y nginx
 echo REACT_APP_API="${api-ip}" >> /etc/environment
 source /etc/environment
-# sudo mkdir /home/ubuntu/final-project-app
 mkdir /home/ubuntu/final-project-app
 sudo chown -R ubuntu /home/ubuntu/final-project-app
 sudo git clone "https://github.com/Johng117/final-project-app.git" "/home/ubuntu/final-project-app"
@@ -16,7 +15,5 @@ cd /home/ubuntu/final-project-app
 npm install
 sudo chmod 777 /home/ubuntu/final-project-app/node_modules
 sudo chmod 777 /home/ubuntu/final-project-app/node_modules/.cache/.eslintcache
-# npm run build --API="${api-ip}"
-cd /etc/nginx/conf.d && touch fp.conf
-sudo chmod 777 /etc/nginx/conf.d/fp.conf
-echo -e "server {\n\tlisten 80;\n\tlisten [::]:80;\n\troot /home/ubuntu/deploy/build;\n\tlocation / {\n\t\ttry_files '$'uri /index.html;\n}\n}" > /etc/nginx/conf.d/fp.conf
+
+
